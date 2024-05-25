6842bd74ebc3862d206c77b3110b61dfcbfa9674
adefrol <adefrolx@gmail.com>
2024-05-25 23:08:10 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  await app.listen(4445);
+  await app.listen(3000);
 }
 bootstrap();
